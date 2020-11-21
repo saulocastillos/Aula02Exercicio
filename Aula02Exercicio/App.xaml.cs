a8818e5bc776c4516c87b5b5d17bdac61f3226ec
Saulo <altfreedom@gmail.com>
2020-11-20 21:44:01 -0300

    final

---
+++ Aula02Exercicio/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aula02Exercicio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CodePlusXamlPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Aula02Exercicio/CodePlusXamlPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aula02Exercicio
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CodePlusXamlPage : ContentPage
+    {
+        public CodePlusXamlPage()
+        {
+            InitializeComponent();
+
+            Label label = new Label
+            {
+                Text = "Hello from Code!",
+                IsVisible = true,
+                Opacity = 0.75,
+                HorizontalTextAlignment = TextAlignment.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Blue,
+                BackgroundColor = Color.FromRgb(255, 128, 128),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic
+            };
+
+            StackLayoutTest.Children.Add(label);
+        }
+    }
+}
+
+
+/**
+ 
+ Label label = new Label 
+            { 
+                Text = "Hello from Code!", 
+                IsVisible = true,
+                Opacity = 0.75, 
+                HorizontalTextAlignment = TextAlignment.Center, 
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                TextColor = Color.Blue,
+                BackgroundColor = Color.FromRgb(255, 128, 128),
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold | FontAttributes.Italic 
+            };
+
+            (Content as StackLayout).Children.Insert(0, label);
+
+*/
