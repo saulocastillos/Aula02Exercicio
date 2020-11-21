404d73b7f428654ad8b28320031234f90d3a3d70
Saulo <altfreedom@gmail.com>
2020-11-20 22:29:49 -0300

    del: comments && del <ScrollView>

--- Aula02Exercicio/CodePlusXamlPage.xaml.cs
+++ Aula02Exercicio/CodePlusXamlPage.xaml.cs
@@ -33,23 +33,3 @@ namespace Aula02Exercicio
         }
     }
 }
-
-
-/**
- 
- Label label = new Label 
-            { 
-                Text = "Hello from Code!", 
-                IsVisible = true,
-                Opacity = 0.75, 
-                HorizontalTextAlignment = TextAlignment.Center, 
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                TextColor = Color.Blue,
-                BackgroundColor = Color.FromRgb(255, 128, 128),
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                FontAttributes = FontAttributes.Bold | FontAttributes.Italic 
-            };
-
-            (Content as StackLayout).Children.Insert(0, label);
-
-*/
